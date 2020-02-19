907fe6bce2f4aa434cb6f9e694d4f767ac6efa36
Elya <elfucio@gmail.com>
2020-02-19 23:21:49 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let arr = [];
+  arr = n.toString().split('').reverse();
+  let num = arr.join('').replace(/\D+/g,"");
+  if (num < 1) {
+      return num * -1;
+  } else return num;  
 }
